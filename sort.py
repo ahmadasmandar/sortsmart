5bbed67407edc468e4264d575937288180c1fbb9
unknown <ahmedasmndr2@gmail.com>
2020-04-11 10:20:59 +0200

    add remove new target

--- sort.py
+++ sort.py
@@ -1,17 +1,25 @@
 import os
 content=os.listdir()
 target="Tutsgalaxy.com"
+target2="FreeCourseWeb.com"
+# print(len(target2))
 
 for item in content:
     # delete the text files and the urls files to clean the library
+    # print(item)
     if (item.find(".txt") > 0 or item.find(".url")> 0 ):
         print(item)
         os.remove(item)
         
-    #rename the files and folders
-    if (item.find(target) >0):
-        # print(len(target))
-        # print(item.find(target))
+    # #rename the files and folders
+    if (item.find(target2) >0):
+        print(item)
+        # print(item.find(target2))
+        new_name=item[22:]
+        # print(new_name)
+        os.rename(item,new_name)
+    elif (item.find(target) >0):
         new_name=item[19:]
+        # print(new_name)
         os.rename(item,new_name)
     
