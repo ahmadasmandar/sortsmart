36f6e246b8b54e2f6f8f8e779937b0049f0c9c29
unknown <ahmedasmndr2@gmail.com>
2020-04-12 14:36:29 +0200

    chosse the dir to work with tk inter jupyter out

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello World!!")
+import os
+# use the tkinter filedialog to choose the dir to work on
+import tkinter as tk
+from tkinter import filedialog
+
+
+root=tk.Tk()
+root.withdraw()
+file_path=filedialog.askdirectory()
+new_file="ahmad.txt"
+make_it=os.path.join(file_path,new_file)
+print(file_path)
+with open(make_it,"w") as f:
+    f.write("Hello World!")
+    f.close()
+
